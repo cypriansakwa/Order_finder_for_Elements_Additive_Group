3276f0b18ac859d97a5c2fc93684b4f097c2f514
DR. Cyprian Omukhwaya Sakwa <cypriansakwa@gmail.com>
2024-08-12 08:07:05 +0300

    old file

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,26 @@ use num_traits::{One, Zero};
 
 fn find_order(n: &BigUint, a: &BigUint) -> BigUint {
     let mut k = BigUint::one();
-    while k <= *n {
-        if (k.clone() * a) % n == BigUint::zero() {
-            return k;
-        }
+    let mut sum = a.clone();
+
+    while &sum % n != BigUint::zero() {
         k += BigUint::one();
+        sum += a;
     }
-    n.clone()
+
+    k
 }
 
 fn main() {
-    let n = BigUint::parse_bytes(b"9999922", 10).unwrap(); // Change this value to any positive integer to represent Z_n
+    let n = BigUint::parse_bytes(b"8", 10).unwrap(); // Change this value to any positive integer to represent Z_n
 
-    for a in 0..=10 {
+    for a in 0..n.to_u32_digits()[0] {
         let a_biguint = BigUint::from(a as u32);
         let order = find_order(&n, &a_biguint);
         println!("Order of {} in Z_{} is {}", a, n, order);
     }
 }
+
 
 
 
